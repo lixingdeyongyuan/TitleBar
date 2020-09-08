81b86df5f99822495a1feae354ff0d1a874166ae
lixingdeyongyuan <lixingdeyongyuan@gmail.com>
2020-09-08 20:43:04 +0800

    通用按钮添加右按钮样式

--- titlebarlib/src/main/java/com/aixue/titlebarlib/CommonTitleBar.kt
+++ titlebarlib/src/main/java/com/aixue/titlebarlib/CommonTitleBar.kt
@@ -8,6 +8,7 @@ import android.view.View
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.layout_title_bar.view.*
 import kotlinx.android.synthetic.main.layout_title_left.view.*
+import kotlinx.android.synthetic.main.layout_title_right.view.*
 
 open class CommonTitleBar @JvmOverloads constructor(
     context: Context?,
@@ -29,6 +30,18 @@ open class CommonTitleBar @JvmOverloads constructor(
             tvLeft.text = titleLeftText
             tvLeft.setTextSize(TypedValue.COMPLEX_UNIT_PX, titleLeftSize)
             tvLeft.setTextColor(titleLeftColor)
+
+            var titleRightText = typedArray.getString(R.styleable.TitleBar_titleRightText)
+            var titleRightSize =
+                typedArray.getDimension(R.styleable.TitleBar_titleRightSize, dip2px(context, 16f))
+            var titleRightColor = typedArray.getColor(
+                R.styleable.TitleBar_titleRightColor,
+                ContextCompat.getColor(context, android.R.color.white)
+            )
+            View.inflate(context,R.layout.layout_title_right,title_right)
+            tvRight.text = titleRightText
+            tvRight.setTextSize(TypedValue.COMPLEX_UNIT_PX, titleRightSize)
+            tvRight.setTextColor(titleRightColor)
         }
     }
 
